9d99c2d19422bfbe16efa91c93d4e5da147f2174
Matt Mundell <matt@mundell.me>
2026-04-04 15:07:14 +0200

    narrowest-scope clean test

--- test/rules/narrowest-scope.js
+++ test/rules/narrowest-scope.js
@@ -1,41 +1,71 @@
 import { RuleTester } from 'eslint'
 import { plugins } from '../../index.js'
 
-let long1, long2, long3, long4, long5, ruleTester
+let ruleTester, validCases, invalidCases
 
 ruleTester = new RuleTester()
+validCases = []
+invalidCases = []
 
-// this is too hard to detect (needs data flow analysis)
-long1 = `
-function f
-(a, b, otherwise) {
-  let c1, ok
+function pass
+(code) {
+  validCases.push({ code })
+}
 
-  if (a)
-    ok = 4
-  else
-    ok = 1
+function fail
+(count, code) {
+  let errors
 
-  if (b) {
-    b.forEach(d => {
-      c1 = c1 || ok
-      c1 += d
-    })
-    return c1
+  errors = []
+
+  while (count > 0) {
+    errors.push({ messageId: 'tooBroad' })
+    count--
   }
+  invalidCases.push({ code, errors })
+}
 
-  {
-    c1 = ok
-    c1 += ok * ok
-    if (c1 > 22)
-      return ok
+pass('function foo() { let x; x = 1; return x }')
+
+pass('for (let i = 0; i < 10; i++) { console.log(i) }')
+
+pass('function outer() { let x; function inner() { x = 1 } return x }')
+
+pass('let x; function foo() { x = 1 } function bar() { return x }')
+
+pass(`
+function f(s1, s2, otherwise) {
+  let a, s
+
+  a = []
+  s = s1
+  while (s) {
+    if (s.done)
+      break
+    a.push(s)
+    s = s.next
   }
-
+  s = s2
+  while (s) {
+    if (s.done)
+      break
+    if (a.includes(s))
+      return s
+    s = s.next
+  }
   return otherwise
 }
-`
+`)
 
-long2 = `
+pass('import { a } from \'a.js\'; { a.f() }')
+
+pass('function foo() { return 1 } function bar() { return foo() }')
+
+fail(1, 'let x = 1; function foo() { return x }')
+
+fail(1, 'let x; { let y = 1; x = y }')
+
+fail(2, `
 function f
 (a, b, otherwise) {
   let c1, c2, ok
@@ -62,80 +92,10 @@ function f
 
   return otherwise
 }
-`
-
-long3 = `
-function f(s1, s2, otherwise) {
-  let a, s
-
-  a = []
-  s = s1
-  while (s) {
-    if (s.done)
-      break
-    a.push(s)
-    s = s.next
-  }
-  s = s2
-  while (s) {
-    if (s.done)
-      break
-    if (a.includes(s))
-      return s
-    s = s.next
-  }
-  return otherwise
-}
-`
-
-long4 = `
-let tout
-
-function update
-(view) {
-  if (tout)
-    clearTimeout(tout)
-  tout = setTimeout(() => console.log('hi'), 10000)
-}
-`
-
-long5 = `
-function init
-() {
-  let tout
-
-  function update
-  (view) {
-    if (tout)
-      clearTimeout(tout)
-    tout = setTimeout(() => console.log('hi'), 10000)
-  }
-}
-`
+`)
 
 globalThis.describe('narrowest-scope',
-                    () => {
-                      ruleTester.run('narrowest-scope',
-                                     plugins.cookshack.rules['narrowest-scope'],
-                                     { valid: [ { code: 'function foo() { let x; x = 1; return x }' },
-                                                { code: 'for (let i = 0; i < 10; i++) { console.log(i) }' },
-                                                { code: 'function outer() { let x; function inner() { x = 1 } return x }' },
-                                                { code: 'let x; function foo() { x = 1 } function bar() { return x }' },
-                                                { code: long3 },
-                                                ...(0 ? { code: long4 } : []),
-                                                ...(0 ? { code: long5 } : []),
-                                                { code: 'import { a } from \'a.js\'; { a.f() }' },
-                                                // laxer on functions for now
-                                                { code: 'function foo() { return 1 } function bar() { return foo() }' } ],
-                                       invalid: [ { code: 'let x = 1; function foo() { return x }',
-                                                    errors: [ { messageId: 'tooBroad' } ] },
-                                                  { code: 'let x; { let y = 1; x = y }',
-                                                    errors: [ { messageId: 'tooBroad' } ] },
-                                                  ...(0
-                                                      ? [ { code: long1,
-                                                            errors: [ { messageId: 'tooBroad' } ] } ]
-                                                      : []),
-                                                  { code: long2,
-                                                    errors: [ { messageId: 'tooBroad' },
-                                                              { messageId: 'tooBroad' } ] } ] })
-                    })
+                    () => ruleTester.run('narrowest-scope',
+                                         plugins.cookshack.rules['narrowest-scope'],
+                                         { valid: validCases,
+                                           invalid: invalidCases }))
