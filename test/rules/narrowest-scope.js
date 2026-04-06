af968988f423fb5aee2a54ee964dfe988f24e76e
Matt Mundell <matt@mundell.me>
2026-04-06 14:56:57 +0200

    narrowest-scope prep expected

--- test/rules/narrowest-scope.js
+++ test/rules/narrowest-scope.js
@@ -44,21 +44,87 @@ function _fail(count, code) {
   throw new Error('expected ' + count + ' errors, got ' + messages.length)
 }
 
-pass('if (g) { let x = 0; x++; console.log(x); }')
+pass('if (g) { let x = 0; x++; console.log(x); }',
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+SCOPE 1.1.1 BLOCK
+LET   x   pos 13
+WRITE x   pos 13.4
+READ  x   pos 20
+WRITE x   pos 20
+READ  x   pos 37`)
 
-pass('let x = 0; x++; console.log(x);')
+pass('let x = 0; x++; console.log(x);',
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   x   pos 4
+WRITE x   pos 4.4
+READ  x   pos 11
+WRITE x   pos 11
+READ  x   pos 28`)
 
-pass('{ let x = 0; if (x) console.log(1); }')
+pass('{ let x = 0; if (x) console.log(1); }',
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+SCOPE 1.1.1 BLOCK
+LET   x   pos 6
+WRITE x   pos 6.4
+READ  x C pos 17`)
 
-pass('let x = 0; x++; let y = x; console.log(y);')
+pass('let x = 0; x++; let y = x; console.log(y);',
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   x   pos 4
+WRITE x   pos 4.4
+READ  x   pos 11
+WRITE x   pos 11
+READ  x   pos 19.6
+LET   y   pos 20
+WRITE y   pos 20.4
+READ  y   pos 39`)
 
-pass('function foo() { let x; x = 1; return x }')
+pass('function foo() { let x; x = 1; return x }',
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   foo   pos 9
+SCOPE 1.1.1 FUNCTION
+LET   x   pos 21
+WRITE x   pos 24
+READ  x   pos 38`)
 
-pass('for (let i = 0; i < 10; i++) { console.log(i) }')
+pass('for (let i = 0; i < 10; i++) { console.log(i) }',
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+SCOPE 1.1.1 FOR
+LET   i   pos 9
+WRITE i   pos 9.4
+READ  i   pos 16
+READ  i   pos 24
+WRITE i   pos 24
+SCOPE 1.1.1.1 BLOCK
+READ  i   pos 43`)
 
-pass('function outer() { let x; function inner() { x = 1 } return x }')
+pass('function outer() { let x; function inner() { x = 1 } return x }',
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   outer   pos 9
+SCOPE 1.1.1 FUNCTION
+LET   x   pos 23
+LET   inner   pos 35
+READ  x   pos 60
+SCOPE 1.1.1.1 FUNCTION
+WRITE x   pos 45`)
 
-pass('let x; function foo() { x = 1 } function bar() { return x }')
+pass('let x; function foo() { x = 1 } function bar() { return x }',
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   x   pos 4
+LET   foo   pos 16
+LET   bar   pos 41
+SCOPE 1.1.1 FUNCTION
+WRITE x   pos 24
+SCOPE 1.1.2 FUNCTION
+READ  x   pos 56`)
 
 pass(`
 function f(s1, s2, otherwise) {
@@ -82,11 +148,53 @@ function f(s1, s2, otherwise) {
   }
   return otherwise
 }
-`)
+`,
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   f   pos 10
+SCOPE 1.1.1 FUNCTION
+LET   s1   pos 12
+LET   s2   pos 16
+LET   otherwise   pos 20
+LET   a   pos 39
+LET   s   pos 42
+WRITE a   pos 47
+WRITE s   pos 59.6
+READ  s1   pos 60
+READ  s   pos 72
+WRITE s   pos 143.6
+READ  s2   pos 144
+READ  s   pos 156
+READ  otherwise   pos 255
+SCOPE 1.1.1.1 BLOCK
+READ  s B pos 85
+READ  a   pos 109
+READ  s   pos 116
+WRITE s   pos 123
+READ  s   pos 127
+SCOPE 1.1.1.2 BLOCK
+READ  s B pos 169
+READ  a B pos 197
+READ  s B pos 208
+READ  s B pos 225
+WRITE s   pos 231
+READ  s   pos 235`)
 
-pass('import { a } from \'a.js\'; { a.f() }')
+pass('import { a } from \'a.js\'; { a.f() }',
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   a   pos 9
+SCOPE 1.1.1 BLOCK
+READ  a   pos 28`)
 
-pass('function foo() { return 1 } function bar() { return foo() }')
+pass('function foo() { return 1 } function bar() { return foo() }',
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   foo   pos 9
+LET   bar   pos 37
+SCOPE 1.1.1 FUNCTION
+SCOPE 1.1.2 FUNCTION
+READ  foo   pos 52`)
 
 pass(`
 let tout
@@ -97,7 +205,17 @@ function update
     clearTimeout(tout)
   tout = setTimeout(() => console.log('hi'), 10000)
 }
-`)
+`,
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   tout   pos 5
+LET   update   pos 20
+SCOPE 1.1.1 FUNCTION
+LET   view   pos 28
+READ  tout C pos 42
+READ  tout B pos 65
+WRITE tout   pos 73
+SCOPE 1.1.1.1 FUNCTION`)
 
 pass(`
 function init
@@ -111,7 +229,19 @@ function init
     tout = setTimeout(() => console.log('hi'), 10000)
   }
 }
-`)
+`,
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   init   pos 10
+SCOPE 1.1.1 FUNCTION
+LET   tout   pos 26
+LET   update   pos 43
+SCOPE 1.1.1.1 FUNCTION
+LET   view   pos 53
+READ  tout C pos 69
+READ  tout B pos 94
+WRITE tout   pos 104
+SCOPE 1.1.1.1.1 FUNCTION`)
 
 pass(`
 function init
@@ -134,9 +264,32 @@ function init
 
   return f
 }
-`)
+`,
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   init   pos 10
+SCOPE 1.1.1 FUNCTION
+LET   stopTimeout   pos 26
+LET   f   pos 50
+READ  f   pos 321
+SCOPE 1.1.1.1 FUNCTION
+READ  stopTimeout C pos 64
+SCOPE 1.1.1.1.1 BLOCK
+READ  stopTimeout   pos 98
+WRITE stopTimeout   pos 117
+SCOPE 1.1.1.1.2 BLOCK
+WRITE stopTimeout   pos 191
+SCOPE 1.1.1.1.2.1 FUNCTION
+WRITE stopTimeout   pos 232`)
 
-pass('try { f() } catch (err) { console.log(err.message) }')
+pass('try { f() } catch (err) { console.log(err.message) }',
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+SCOPE 1.1.1 BLOCK
+SCOPE 1.1.2 CATCH
+LET   err   pos 19
+SCOPE 1.1.2.1 BLOCK
+READ  err   pos 38`)
 
 pass(`
   class A extends B {
@@ -148,11 +301,36 @@ pass(`
   }
 
   g = f({ a() { return new A('eg') } })
-`)
+`,
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   A   pos 9
+SCOPE 1.1.1 CLASS
+LET   A   pos 9
+SCOPE 1.1.1.1 FUNCTION
+LET   name   pos 44
+READ  name   pos 84
+SCOPE 1.1.2 FUNCTION
+READ  A   pos 127`)
 
-fail(1, 'let x = 1; function foo() { return x }')
+fail(1, 'let x = 1; function foo() { return x }',
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   x   pos 4
+WRITE x   pos 4.4
+LET   foo   pos 20
+SCOPE 1.1.1 FUNCTION
+READ  x   pos 35`)
 
-fail(1, 'let x; { let y = 1; x = y }')
+fail(1, 'let x; { let y = 1; x = y }',
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   x   pos 4
+SCOPE 1.1.1 BLOCK
+LET   y   pos 13
+WRITE y   pos 13.4
+WRITE x   pos 23.6
+READ  y   pos 24`)
 
 fail(2, `
 function f
@@ -181,9 +359,50 @@ function f
 
   return otherwise
 }
-`)
+`,
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   f   pos 10
+SCOPE 1.1.1 FUNCTION
+LET   a   pos 13
+LET   b   pos 16
+LET   otherwise   pos 19
+LET   c1   pos 38
+LET   c2   pos 42
+LET   ok   pos 46
+READ  a C pos 56
+WRITE ok B pos 63
+WRITE ok B pos 81
+READ  b C pos 95
+READ  otherwise   pos 253
+SCOPE 1.1.1.1 BLOCK
+WRITE c1   pos 104
+READ  b   pos 115
+READ  c1   pos 164
+SCOPE 1.1.1.1.1 FUNCTION
+LET   d   pos 125
+WRITE c1   pos 143.6
+READ  d   pos 144
+SCOPE 1.1.1.2 BLOCK
+WRITE c2   pos 184.6
+READ  ok   pos 185
+WRITE c2   pos 192
+READ  ok   pos 198
+READ  ok   pos 203
+READ  c2 B pos 214
+READ  ok B pos 236`)
 
-fail(1, 'let a; try { f() } catch (err) { a = err.message; console.log(a) }')
+fail(1, 'let a; try { f() } catch (err) { a = err.message; console.log(a) }',
+     `SCOPE 1 GLOBAL
+SCOPE 1.1 MODULE
+LET   a   pos 4
+SCOPE 1.1.1 BLOCK
+SCOPE 1.1.2 CATCH
+LET   err   pos 26
+SCOPE 1.1.2.1 BLOCK
+WRITE a   pos 33
+READ  err   pos 37
+READ  a   pos 62`)
 
 globalThis.describe('narrowest-scope', () => {
   for (let tc of validCases)
