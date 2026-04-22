91af34abe25c66986c9b416e59d1cbd2162f2827
Matt Mundell <matt@mundell.me>
2026-04-22 22:19:47 +0200

    decl-block-start fn decl

--- plugins/decl-block-start.js
+++ plugins/decl-block-start.js
@@ -27,5 +27,29 @@ default { meta: { type: 'suggestion',
                   return
                 }
               }
-            } }
+            },
+                     FunctionDeclaration(node) {
+                       let parent
+
+                       for (parent = node.parent; parent; parent = parent.parent)
+                         if (parent.type == 'BlockStatement')
+                           break
+
+                       if (parent) {
+                         let idx
+
+                         if (parent.parent?.type == 'CatchClause')
+                           return
+
+                         idx = parent.body.indexOf(node)
+                         for (let i = 0; i < idx; i++) {
+                           if (parent.body[i].type == 'VariableDeclaration'
+                               || parent.body[i].type == 'FunctionDeclaration'
+                               || parent.body[i].type == 'EmptyStatement')
+                             continue
+                           context.report({ node, messageId: 'declBlockStart' })
+                           return
+                         }
+                       }
+                     } }
           } }
