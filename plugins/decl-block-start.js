658b7d97c837c329ef080cfd5e75afc6c32e9666
Matt Mundell <matt@mundell.me>
2026-04-22 22:23:10 +0200

    decl-block-start clean

--- plugins/decl-block-start.js
+++ plugins/decl-block-start.js
@@ -1,55 +1,66 @@
+function
+VariableDeclaration
+(context, node) {
+  let parent
+
+  parent = node.parent
+
+  for (parent = node.parent; parent; parent = parent.parent)
+    if (parent.type == 'BlockStatement')
+      break
+
+  if (parent) {
+    let idx
+
+    if (parent.parent?.type == 'CatchClause')
+      return
+
+    idx = parent.body.indexOf(node)
+    for (let i = 0; i < idx; i++) {
+      if (parent.body[i].type == 'VariableDeclaration')
+        continue
+      context.report({ node, messageId: 'declBlockStart' })
+      return
+    }
+  }
+}
+
+function
+FunctionDeclaration
+(context, node) {
+  let parent
+
+  for (parent = node.parent; parent; parent = parent.parent)
+    if (parent.type == 'BlockStatement')
+      break
+
+  if (parent) {
+    let idx
+
+    if (parent.parent?.type == 'CatchClause')
+      return
+
+    idx = parent.body.indexOf(node)
+    for (let i = 0; i < idx; i++) {
+      if (parent.body[i].type == 'VariableDeclaration'
+          || parent.body[i].type == 'FunctionDeclaration'
+          || parent.body[i].type == 'EmptyStatement')
+        continue
+      context.report({ node, messageId: 'declBlockStart' })
+      return
+    }
+  }
+}
+
+function create
+(context) {
+  return { VariableDeclaration: node => VariableDeclaration(context, node),
+           FunctionDeclaration: node => FunctionDeclaration(context, node) }
+}
+
 export
 default { meta: { type: 'suggestion',
                   docs: { description: 'Require variable declarations to be at the start of the block.' },
                   messages: { declBlockStart: 'Variable declarations must be at the start of the block.' },
                   schema: [] },
-          create(context) {
-            return { VariableDeclaration(node) {
-              let parent
-
-              parent = node.parent
-
-              for (parent = node.parent; parent; parent = parent.parent)
-                if (parent.type == 'BlockStatement')
-                  break
-
-              if (parent) {
-                let idx
-
-                if (parent.parent?.type == 'CatchClause')
-                  return
-
-                idx = parent.body.indexOf(node)
-                for (let i = 0; i < idx; i++) {
-                  if (parent.body[i].type == 'VariableDeclaration')
-                    continue
-                  context.report({ node, messageId: 'declBlockStart' })
-                  return
-                }
-              }
-            },
-                     FunctionDeclaration(node) {
-                       let parent
-
-                       for (parent = node.parent; parent; parent = parent.parent)
-                         if (parent.type == 'BlockStatement')
-                           break
-
-                       if (parent) {
-                         let idx
-
-                         if (parent.parent?.type == 'CatchClause')
-                           return
-
-                         idx = parent.body.indexOf(node)
-                         for (let i = 0; i < idx; i++) {
-                           if (parent.body[i].type == 'VariableDeclaration'
-                               || parent.body[i].type == 'FunctionDeclaration'
-                               || parent.body[i].type == 'EmptyStatement')
-                             continue
-                           context.report({ node, messageId: 'declBlockStart' })
-                           return
-                         }
-                       }
-                     } }
-          } }
+          create }
