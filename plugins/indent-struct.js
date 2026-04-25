b39a4f892c59a5ad90fa22c2f8128cfcf8c0d26b
Matt Mundell <matt@mundell.me>
2026-04-25 14:16:56 +0200

    indent-struct trace

--- plugins/indent-struct.js
+++ plugins/indent-struct.js
@@ -1,3 +1,9 @@
+function trace
+(...args) {
+  if (0)
+    console.log(...args)
+}
+
 function unit
 (context) {
   return context.options[0] ?? 2
@@ -18,14 +24,13 @@ function checkObjectExpressionProperties
   afterLastProp = sourceCode.slice(lastPropEnd, closingBrace)
   lastPropValueEndLine = sourceCode.slice(0, lastPropEnd).split('\n').length
 
-  console.log('ALGORITHM 1: Single-line objects: always valid (no check needed)')
-  console.log('CHECK POINT 1: is single-line? firstPropLine=%d, braceLine=%d, closingLine=%d', firstPropLine, node.loc.start.line, closingLine)
-  console.log('DEBUG: sourceCode starts with=%j', sourceCode.slice(0, 50))
+  trace('ALGORITHM 1: Single-line objects: always valid (no check needed)')
+  trace('CHECK POINT 1: is single-line? firstPropLine=%d, braceLine=%d, closingLine=%d', firstPropLine, node.loc.start.line, closingLine)
   if (firstPropLine == node.loc.start.line && closingLine == firstPropLine)
     return
 
-  console.log('ALGORITHM 2: When first property is on a line after {: must use 1 indent unit from the column where { appears')
-  console.log('CHECK POINT 2: is firstPropLine (%d) != braceLine (%d)?', firstPropLine, node.loc.start.line)
+  trace('ALGORITHM 2: When first property is on a line after {: must use 1 indent unit from the column where { appears')
+  trace('CHECK POINT 2: is firstPropLine (%d) != braceLine (%d)?', firstPropLine, node.loc.start.line)
   if (firstPropLine == node.loc.start.line) {
     // first property is on brace line
   }
@@ -33,12 +38,12 @@ function checkObjectExpressionProperties
     // ok
   }
   else {
-    console.log('CHECK 2 FAIL')
+    trace('CHECK 2 FAIL')
     context.report({ node: firstProp, messageId: 'indentStruct' })
   }
 
-  console.log('ALGORITHM 3: Multi-line: all properties must align to first propertys column')
-  console.log('CHECK POINT 3: checking property alignment, firstPropCol=%d', firstPropCol)
+  trace('ALGORITHM 3: Multi-line: all properties must align to first propertys column')
+  trace('CHECK POINT 3: checking property alignment, firstPropCol=%d', firstPropCol)
   for (let i = 1; i < properties.length; i++) {
     let prop
 
@@ -47,43 +52,43 @@ function checkObjectExpressionProperties
       // ok
     }
     else {
-      console.log('CHECK 3 FAIL')
+      trace('CHECK 3 FAIL')
       context.report({ node: prop, messageId: 'indentStruct' })
     }
   }
 
-  console.log('ALGORITHM 4: } on a line following the last prop value must align with {')
-  console.log('CHECK POINT 4: is closing on a line following last prop value? closingLine=%d, lastPropValueEndLine=%d', closingLine, lastPropValueEndLine)
+  trace('ALGORITHM 4: } on a line following the last prop value must align with {')
+  trace('CHECK POINT 4: is closing on a line following last prop value? closingLine=%d, lastPropValueEndLine=%d', closingLine, lastPropValueEndLine)
   if (closingLine > lastPropValueEndLine) {
     let braceCol, closingCol
 
     braceCol = node.loc.start.column
     closingCol = node.loc.end.column - 1
-    console.log('CHECK 4: braceCol=%d, closingCol=%d', braceCol, closingCol)
+    trace('CHECK 4: braceCol=%d, closingCol=%d', braceCol, closingCol)
     if (closingCol == braceCol) {
       // ok
     }
     else {
-      console.log('CHECK 4 FAIL')
+      trace('CHECK 4 FAIL')
       context.report({ node, messageId: 'indentStruct' })
     }
   }
 
-  console.log('ALGORITHM 5: } on same line as last prop value must have a space before it')
-  console.log('CHECK POINT 5: is closing on same line as last prop value? closingLine=%d, lastPropValueEndLine=%d', closingLine, lastPropValueEndLine)
+  trace('ALGORITHM 5: } on same line as last prop value must have a space before it')
+  trace('CHECK POINT 5: is closing on same line as last prop value? closingLine=%d, lastPropValueEndLine=%d', closingLine, lastPropValueEndLine)
   if (closingLine == lastPropValueEndLine) {
-    console.log('CHECK 5: lastPropEnd=%d, closingBrace=%d, afterLastProp=%j', lastPropEnd, closingBrace, afterLastProp)
+    trace('CHECK 5: lastPropEnd=%d, closingBrace=%d, afterLastProp=%j', lastPropEnd, closingBrace, afterLastProp)
     if (afterLastProp == ' ') {
       // ok
     }
     else {
-      console.log('CHECK 5 FAIL')
+      trace('CHECK 5 FAIL')
       context.report({ node, messageId: 'indentStruct' })
     }
   }
 
-  console.log('ALGORITHM 6: When a param list is on a line after the field name, it must align with the field name')
-  console.log('CHECK POINT 6: checking method param alignment')
+  trace('ALGORITHM 6: When a param list is on a line after the field name, it must align with the field name')
+  trace('CHECK POINT 6: checking method param alignment')
   for (let prop of properties)
     if (prop.method) {
       let keyLine, keyEnd, i, newlines, parenLine
@@ -109,7 +114,7 @@ function checkObjectExpressionProperties
           // ok
         }
         else {
-          console.log('CHECK 6 FAIL')
+          trace('CHECK 6 FAIL')
           context.report({ node: prop, messageId: 'indentStruct' })
         }
       }
